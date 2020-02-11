a384edb283fe3a6fb5dd6704c07915c57315533d
Alexander Öberg <stativetroller7@gmail.com>
2020-02-11 10:20:48 +0100

    camera iso

---
+++ GameJam2020_3D/GameJam2020_3D/IsometricCamera.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ArcBallCamera
+{
+    class IsometricCamera : Camera
+    {
+
+        // Distance between target and camera
+        public float Distance { get; set; }
+
+        // Boundaries for distance
+        public float MinDistance { get; set; }
+        public float MaxDistance { get; set; }
+
+        public Vector3 Position { get; private set; }
+        public Vector3 Target { get; set; }
+
+        public IsometricCamera(Vector3 target, float distance, float minDistance, float maxDistance, GraphicsDevice graphicsDevice)
+            : base(graphicsDevice, ProjectionMatrixType.Orthographic)
+        {
+            // Clamp Distance
+            Distance = MathHelper.Clamp(distance, minDistance, maxDistance);
+            Target = target;
+
+            Update();
+            
+        }
+
+        public void Move(float distanceChange)
+        {
+            Distance += distanceChange;
+            Distance = MathHelper.Clamp(Distance, MinDistance, MaxDistance);
+        }
+
+        public void Rotate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Zoom(float distance)
+        {
+            Distance = distance;
+            Distance = MathHelper.Clamp(Distance, MinDistance, MaxDistance);
+
+        }
+
+        public override void Update()
+        {
+            Matrix rotationMatrix = Matrix.CreateRotationX((float)Math.Asin(Math.Tan(MathHelper.ToRadians(30f)))) * Matrix.CreateRotationY(MathHelper.ToRadians(45f));
+
+            Vector3 transform = Vector3.Transform(new Vector3(0,0,Distance), rotationMatrix);
+
+            Position = Target + transform;
+            View = Matrix.CreateLookAt(Position, Target, Vector3.Up);
+        }
+
+    }
+}
--- Tools_XNA_dotNET_Framework/Tools_XNA_dotNET_Framework/IsometricCamera.cs
+++ Tools_XNA_dotNET_Framework/Tools_XNA_dotNET_Framework/IsometricCamera.cs
@@ -6,10 +6,86 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Tools_XNA
 {
-    public class IsometricCamera : Camera
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    namespace Tools_XNA
     {
-        public IsometricCamera(GraphicsDevice graphicsDevice) : base(graphicsDevice)
+        public class IsometricCamera
         {
+            Matrix view;
+            Matrix projection;
+
+            public Matrix Projection
+            {
+                get { return projection; }
+                protected set
+                {
+                    projection = value;
+                    generateFrustum();
+                }
+            }
+
+            public Matrix View
+            {
+                get { return view; }
+                protected set
+                {
+                    view = value;
+                    generateFrustum();
+                }
+            }
+
+            // Create frustum
+            public BoundingFrustum Frustum { get; private set; }
+
+            protected GraphicsDevice GraphicsDevice { get; set; }
+
+            // Constructor
+            public IsometricCamera(GraphicsDevice graphicsDevice)
+            {
+                this.GraphicsDevice = graphicsDevice;
+
+                generateProjectionMatrix(MathHelper.PiOver4);
+            }
+
+            // Converting the camera view to an 2D image
+            private void generateProjectionMatrix(float FieldOfView)
+            {
+                // Load window (2D viewport) information
+                PresentationParameters pp = GraphicsDevice.PresentationParameters;
+
+                // Aspect ratio is width divided by height
+                float aspectRatio = (float)pp.BackBufferWidth / (float)pp.BackBufferHeight;
+
+
+                this.projection = Matrix.CreateOrthographic(GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height, 0.01f, 100000.0f);
+            }
+
+            // "Open" update method for any class to override
+            public virtual void Update()
+            {
+
+            }
+
+            // Generate frustum that acts as a "render frustum", everything within frustum will be rendered, which is based on view and projection
+            private void generateFrustum()
+            {
+                Matrix viewProjection = View * Projection;
+                Frustum = new BoundingFrustum(viewProjection);
+            }
+
+            // Look if spheres is within frustum
+            public bool BoundingVolumeIsInView(BoundingSphere sphere)
+            {
+                return (Frustum.Contains(sphere) != ContainmentType.Disjoint);
+            }
+
+            // Look if boxes is within frustum
+            public bool BoundingVolumeIsInView(BoundingBox box)
+            {
+                return (Frustum.Contains(box) != ContainmentType.Disjoint);
+            }
         }
     }
 }
